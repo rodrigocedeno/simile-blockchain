f71787bfc8588d54d13dce46e7823a76230bc847
rodrigocedeno <rodrigo.cedeno.j@gmail.com>
2022-04-26 17:38:33 +0200

    update

--- nuxt.config.js
+++ nuxt.config.js
@@ -46,6 +46,7 @@ export default {
   modules: [
     // https://go.nuxtjs.dev/axios
     '@nuxtjs/axios',
+    'nuxt-session',
   ],
 
   // Axios module configuration: https://go.nuxtjs.dev/config-axios
